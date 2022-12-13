8f43a3638bf2621c307dea56bf10f9b6c2cfd3dd
matotubol <matotubol@gmail.com>
2022-12-13 19:50:32 +0100

    Added durability to trimming_scissors
    Made check if enough durability else make the reward less by the missing durability fo example the reward is 100 and the durability is -5 every plant. If we take 10 plants for example, that will make it -50 durability but if the owned scissors have dirability of 40 then the missing 10  will be taken from the reward so the new reward is 90

--- server/weed-process.lua
+++ server/weed-process.lua
@@ -2,11 +2,29 @@ RegisterServerEvent('mato-drugs:completeProccess')
 
 AddEventHandler('mato-drugs:completeProccess', function (rewardCount, plantCount)
 
-    local inventory = exports.ox_inventory:GetInventory(source, false)
+    local inventory = exports.ox_inventory:GetInventory(source, true)
     local freeWeight = inventory.maxWeight - inventory.weight + (exports.ox_inventory:GetItem(source, 'cannabis', nil, false).weight * plantCount)
+    local inventoryScissors = exports.ox_inventory:Search(source, 'slots', 'trimming_scissors')
     local weedBagsAmount = 0 -- 3.5 oz bag
+
+    for k, v in pairs(inventoryScissors) do
+        inventoryScissors = v
+        break
+    end
+
+    if inventoryScissors.slot ~= nil then
+        if inventoryScissors.metadata.durability - 5 * plantCount > 0 then
+            inventoryScissors.metadata.durability -= 5 * plantCount
+            exports.ox_inventory:SetMetadata(source, inventoryScissors.slot, inventoryScissors.metadata)
+        else
+            rewardCount -= math.abs(inventoryScissors.metadata.durability - (5 * plantCount))
+            exports.ox_inventory:RemoveItem(source, 'trimming_scissors', 1, inventoryScissors.metadata, inventoryScissors.slot)
+
+        end
+    end
+
     if rewardCount >= 100 and freeWeight >= 100 then
-        for i = 1, rewardCount do
+        for i = 1, plantCount do
             if rewardCount - 100 >= 0 and freeWeight - 100 >= 0 then 
                 freeWeight  -= 100
                 rewardCount -= 100
@@ -17,10 +35,11 @@ AddEventHandler('mato-drugs:completeProccess', function (rewardCount, plantCount
             end
         end
     end
+    if rewardCount > freeWeight then rewardCount = freeWeight end
+        if rewardCount > 0 then exports.ox_inventory:AddItem(source, 'weed_1g', rewardCount) end
+        exports.ox_inventory:RemoveItem(source, 'cannabis', plantCount)
+        
     if weedBagsAmount > 0 then
         exports.ox_inventory:AddItem(source, 'weed_3.5', weedBagsAmount)
-     end
-    if rewardCount > freeWeight then rewardCount = freeWeight end
-        exports.ox_inventory:AddItem(source, 'weed_1g', rewardCount)
-        exports.ox_inventory:RemoveItem(source, 'cannabis', plantCount)
+    end
 end)
