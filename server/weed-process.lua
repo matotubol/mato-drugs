184c7e6a9f14f79d3a9678224b6646e5e843e904
matotubol <matotubol@gmail.com>
2022-12-13 21:04:02 +0100

    fix..

--- server/weed-process.lua
+++ server/weed-process.lua
@@ -1,7 +1,6 @@
 RegisterServerEvent('mato-drugs:completeProccess')
 
 AddEventHandler('mato-drugs:completeProccess', function (rewardCount, plantCount)
-
     local inventory = exports.ox_inventory:GetInventory(source, true)
     local freeWeight = inventory.maxWeight - inventory.weight + (exports.ox_inventory:GetItem(source, 'cannabis', nil, false).weight * plantCount)
     local inventoryScissors = exports.ox_inventory:Search(source, 'slots', 'trimming_scissors')
@@ -19,17 +18,18 @@ AddEventHandler('mato-drugs:completeProccess', function (rewardCount, plantCount
         else
             rewardCount -= math.abs(inventoryScissors.metadata.durability - (5 * plantCount))
             exports.ox_inventory:RemoveItem(source, 'trimming_scissors', 1, inventoryScissors.metadata, inventoryScissors.slot)
-
         end
     end
-        weedBagsAmount = rewardCount / 100 - rewardCount % 100 -- because 1 bag is 100gram
-        rewardCount    = rewardCount % 100
-
     if rewardCount > freeWeight then rewardCount = freeWeight end
-        if rewardCount > 0 then exports.ox_inventory:AddItem(source, 'weed_1g', rewardCount) end
+        weedBagsAmount = math.floor(rewardCount / 100)
+        rewardCount    = rewardCount % 100 -- gets the reminder
         exports.ox_inventory:RemoveItem(source, 'cannabis', plantCount)
+
+    if rewardCount > 0 then exports.ox_inventory:AddItem(source, 'weed_1g', rewardCount) end
 
     if weedBagsAmount > 0 then
         exports.ox_inventory:AddItem(source, 'weed_3.5', weedBagsAmount)
     end
+
+    print(weedBagsAmount, rewardCount)
 end)
