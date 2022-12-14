339c2e49ced6e2a4017714a6a7dce6b56ee06ed0
matotubol <matotubol@gmail.com>
2022-12-14 05:43:02 +0100

    optimize level system

--- server/weed-process.lua
+++ server/weed-process.lua
@@ -1,19 +1,19 @@
 RegisterServerEvent('mato-drugs:completeProccess')
 
-local function calculateLevel(xp)
-    local startLevel = 10
-    local table = {level = 0, nextlevel = 0}
-    for i = 1, xp do
-      if startLevel > xp then
-        table.nextLevel = startLevel
-        break
-      else
-        startLevel *= 2
-        table.level += 1
-      end
-    end  
-        return table
+function calculateLevel(xp)
+    -- Use a logarithmic formula to calculate the player's level
+    local level = math.floor(math.log(xp / 10) / math.log(2)) + 1
+    
+    -- Calculate the xp needed for the next level
+    local nextLevel = 10 * 2^(level - 1)
+    
+    return {level = level, nextLevel = nextLevel}
     end
+  
+  function getLevelPercentage(xp)
+    local level = calculateLevel(xp)
+    return (xp - level.nextLevel) / (level.nextLevel * 2 - level.nextLevel) * 100
+  end
 
 AddEventHandler('mato-drugs:completeProccess', function (rewardCount, plantCount)
     local inventory = exports.ox_inventory:GetInventory(source, true)
@@ -21,7 +21,6 @@ AddEventHandler('mato-drugs:completeProccess', function (rewardCount, plantCount
     local inventoryScissors = exports.ox_inventory:Search(source, 'slots', 'trimming_scissors')
     local weedBagsAmount = 0 -- 3.5 oz bag
     local player = Ox.GetPlayer(source)
-
 
     for k, v in pairs(inventoryScissors) do
         inventoryScissors = v
@@ -51,7 +50,8 @@ AddEventHandler('mato-drugs:completeProccess', function (rewardCount, plantCount
 
     local xpLevel = MySQL.single.await('SELECT weed_xp FROM mato_drugs WHERE identifier = ?', {player.userid})
     if xpLevel then
-        print('Level '..calculateLevel(xpLevel.weed_xp).level, 'Next level XP = '..calculateLevel(xpLevel.weed_xp).nextLevel)
+        print('level', calculateLevel(xpLevel.weed_xp).level, '%'.. getLevelPercentage(xpLevel.weed_xp))
+
         MySQL.update.await('UPDATE mato_drugs SET weed_xp = weed_xp + ? WHERE identifier = ?', {plantCount, player.userid})
     else
         MySQL.insert.await('INSERT INTO mato_drugs (identifier, weed_xp) VALUES (?, ?)', {player.userid, 0})
